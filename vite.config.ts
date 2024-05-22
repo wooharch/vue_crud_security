cf2db3b1f03824acbcfbba28799bb3799fbf9b87
ujeongeom <110080718+ujeongeom@users.noreply.github.com>
2024-05-22 13:47:09 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,30 +10,20 @@ export default defineConfig({
     vue({
       template: { transformAssetUrls }
     }),
-    // @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: 'src/quasar-variables.sass'
     })
   ],
-  //lcocal test 시 주석 삭제 
-  // ---- Start -----
   server: {
     // host: 'd10rrjyt8il5jp.cloudfront.net',
     // port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://k8s-simple-backendi-12183553ad-1493743248.ap-northeast-2.elb.amazonaws.com/',
-        changeOrigin: true
-      },
-      '/login': {
+      '/api/*': {
         target: 'http://k8s-simple-backendi-12183553ad-1493743248.ap-northeast-2.elb.amazonaws.com/',
         changeOrigin: true
       }
     }
   },
-  // ---- end -----
-  //lcocal test 시 주석 삭제 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
