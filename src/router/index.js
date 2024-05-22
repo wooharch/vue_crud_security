a2223b2b45ae04fc59882fa4580b57267f3778bc
ujeongeom <110080718+ujeongeom@users.noreply.github.com>
2024-05-22 14:13:06 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
   linkActiveClass: 'active',
   routes: [
     { path: '/', component: () => import('@/views/Login.vue') },
-    { path: '/login', component: () => import('@/views/Login.vue') },
+    { path: '/api/login', component: () => import('@/views/Login.vue') },
     { path: '/employees', component: () => import('@/views/Employees.vue') },
     {
       path: '/:catchAll(.*)*',
@@ -19,13 +19,13 @@ const router = createRouter({
 
 router.beforeEach(async (to) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/login'];
+  const publicPages = ['/api/login'];
   const authRequired = !publicPages.includes(to.path);
   const authStore = useAuthStore();
 
   if (authRequired && !authStore.user) {
     authStore.returnUrl = to.fullPath;
-    return '/login';
+    return '/api/login';
   }
 });
 
